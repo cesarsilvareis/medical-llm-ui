75d46ad5a00bdc5ca26da72bd4838b179fb414fd
cesarsilvareis <cesar.reis@tecnico.ulisboa.pt>
2024-04-02 11:53:17 +0100

    Added prompt template for medical congress slide builder task

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import streamlit as st
 from streamlit import runtime
 from streamlit.web.cli import main as strunner
 
+from langchain_core.prompts import PromptTemplate
+
+@st.cache_data
+def prompting(task: str) -> PromptTemplate:
+    with open('prompt.txt', 'r') as p:
+        prompt = PromptTemplate.from_template(p.read().rstrip())    
+    return prompt
 
 def streamlit_app():
 
@@ -12,13 +19,64 @@ def streamlit_app():
         page_title="DrGPT - Medical Template",
         page_icon="📓",
         layout="wide",
+        initial_sidebar_state="auto"
     )
 
-    st.write("""
-    # Encapsulating the Prompt Engineering for Medical Users #
+    st.title("Encapsulating the Prompt Engineering for Medical Users")
+
+    result = None
+
+    with st.sidebar:
+        st.write("Configuration:")
+
+        task = st.selectbox(label="Task", options=[
+            "IE Guidelines Congress Slide Builder"
+        ])
+
+        prompt = prompting(task)
+
+        st.divider()
+        theme = st.text_input(label="Theme", value="", max_chars=25)
+
+        presenter_name = st.text_input(label="Presenter Name", value="", max_chars=15)
+        presenter_affiliation = st.text_input(label="Presenter Affiliation", value="", max_chars=15)
+        date_time = st.date_input("Presentation Date", format="DD/MM/YYYY")
+
+        objective = st.text_area(label="Objectives", value="Inform possible changes on IE guidelines")
+        audience = st.text_input(label="Targeted Audience", value="Physicians", max_chars=25)
+
+        tone = st.text_input(label="Tone", value="rigorous", max_chars=20)
+        language = st.selectbox(label="Language", options=["English", "Portuguese"])
+
+        duration = st.number_input(label="Duration (in minutes)")
+        slide_time = st.number_input(label="Expected Slide Time (slides/min)", value=1)
+
+        if st.button(label="Submit"):
+            result = prompt.invoke({
+                "theme": theme,
+                "presenter_name": presenter_name,
+                "presenter_affiliation": presenter_affiliation,
+                "date_time": date_time,
+                "objective": objective,
+                "audience": audience,
+                "tone": tone,
+                "language": language,
+                "duration": duration,
+                "slide_time": slide_time
+            }).to_string()
+
+    # Prompt Placement
+
+    if result is None:
+        st.info("No specific configuration has been established for crafting a new prompt.")
+        return
+
+    st.write(f"""
+    ## Task : {task}
     """)
-
-
+    st.write(result)
+    st.info("Please copy it! And move to [ChatGPT](%s) to prompt it!" %("https://chat.openai.com"))
+             
 
 def main():
     if runtime.exists():
